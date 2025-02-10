eaaa00dbd07ae46275b2f67c650373a6f4d3f060
dbl8005 <142303531+dbl8005@users.noreply.github.com>
2025-02-10 18:53:10 +0200

    Remove unused import from main.dart
    Clean up unnecessary import of CurrentWeatherPage to reduce unused code and improve project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:weather_app/core/theme/app_theme.dart';
 import 'package:weather_app/features/weather/presentation/pages/main_page.dart';
-import 'package:weather_app/features/weather/presentation/pages/current_weather_page.dart';
 
 void main() {
   runApp(const MyApp());
